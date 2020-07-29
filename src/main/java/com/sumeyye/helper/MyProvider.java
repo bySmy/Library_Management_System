ee4bcc232419e001a01ea3810cea7098521550c5
frecebli <frecebli@E-frecebli.turgutozal.edu.tr>
2020-07-29 00:22:23 -0400

    Add project

--- src/main/java/com/sumeyye/helper/MyProvider.java
+++ src/main/java/com/sumeyye/helper/MyProvider.java
@@ -4,5 +4,5 @@ public interface MyProvider {
 
 	String url="jdbc:mysql://localhost:3306/users?serverTimezone=UTC";           
 	String username="root";
-    String password="semendel.79";
+    String password="12345";
 }
